52815d987f8a88b1e41539a216282f4f57b83a1a
Marius <mariuswaseige@protonmail.com>
2024-01-22 11:26:08 +0100

    first commit

---
+++ src/components/Child.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import GrandChild from './GrandChild.vue';
+    import {useNumbersStore} from '../stores/numbers';
+    
+    const {numbers, addNumber} = useNumbersStore();
+
+    
+</script>      
+
+<template>
+
+    <div>
+        <h1>Child Component {{ numbers }}</h1>
+        <button @click="addNumber(4)">Add</button>
+        <div class="line"></div>
+        <GrandChild />
+        
+    </div>
+
+</template>
---
+++ src/components/GreatGrandChild.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+import {useNumbersStore} from '../stores/numbers';
+    
+    const {numbers, filterNum} = useNumbersStore();
+
+</script>      
+
+<template>
+
+    <div>
+
+        <h1>Great GrandChild Component {{ filterNum(6) }}</h1>
+       
+    </div>
+
+</template>
